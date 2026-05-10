726f61fd1be68ab7c29ae20149350307dda72b17
carlos123asd <carlos-medin@hotmail.com>
2026-05-10 19:55:32 +0200

    add: timeout for getProduct

--- src/main/java/com/backtest/backtest/client/ProductApiClient.java
+++ src/main/java/com/backtest/backtest/client/ProductApiClient.java
@@ -7,6 +7,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Component
 @RequiredArgsConstructor
 public class ProductApiClient {
@@ -21,10 +23,16 @@ public class ProductApiClient {
     }
 
     //Objeto ProductDetailsResponse
+    /*
+    * En las pruebas k6 lanza una prueba de rendimiento de 200 usuario tomando en cuenta el mockeado de los endpoints,
+    *  se ha uno con un delay de casi 1 min (50 seg.), por lo que que genera una latencia alta llegado a este punto
+    * por eso se ha establecido un timeout de 2 segundos para evitar esperas prolongadas y mejorar la experiencia del usuario.
+    * */
     public Mono<ProductDetailsResponse> getProduct(String productId) {
         return webClient.get()
                 .uri("/product/{id}", productId)
                 .retrieve()
-                .bodyToMono(ProductDetailsResponse.class);
+                .bodyToMono(ProductDetailsResponse.class)
+                .timeout(Duration.ofSeconds(2)); // Timeout de 2 segundos para evitar esperas prolongadas
     }
 }
